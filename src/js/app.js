bc583c1a3ca95f3aca7e579698647afca6874077
Benji Huggins <benji.huggins@thecroc.com>
2021-05-26 16:08:19 +0100

    Start pinned slideshow

--- src/js/app.js
+++ src/js/app.js
@@ -1,44 +1,74 @@
 import '../scss/app.scss';
 
+let bodyScrollBar = Scrollbar.init(document.body, {
+  damping: 0.1,
+  delegateTo: document,
+});
+ScrollTrigger.scrollerProxy('.scroller', {
+  scrollTop(value) {
+    if (arguments.length) {
+      bodyScrollBar.scrollTop = value;
+    }
+    return bodyScrollBar.scrollTop;
+  },
+});
+bodyScrollBar.addListener(ScrollTrigger.update);
+
 // SLIDER
-// const sections = gsap.utils.toArray('.sticky-slides .slide');
-// let maxWidth = 0;
+gsap.set('.panel', { zIndex: (i, target, targets) => targets.length - i });
 
-// const getMaxWidth = () => {
-//   maxWidth = 0;
-//   sections.forEach((section) => {
-//     maxWidth += section.offsetWidth * 1.172;
-//   });
-// };
-// getMaxWidth();
-// ScrollTrigger.addEventListener('refreshInit', getMaxWidth);
+var images = gsap.utils.toArray('.panel:not(.purple)');
 
-// gsap.to(sections, {
-//   x: () => `-${maxWidth - window.innerWidth}`,
-//   ease: 'none',
-//   scrollTrigger: {
-//     trigger: '#slideshow-wrap',
-//     pin: true,
-//     scrub: true,
-//     end: () => `+=${maxWidth}`,
-//     invalidateOnRefresh: true,
-//   },
-// });
+images.forEach((image, i) => {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#slideshow-wrap',
+      scroller: '.scroller',
+      start: () => 'top -' + window.innerHeight * (i + 0.5),
+      end: () => '+=' + window.innerHeight,
+      scrub: true,
+      toggleActions: 'play none reverse none',
+      invalidateOnRefresh: true,
+    },
+  });
 
-// sections.forEach((sct, i) => {
-//   ScrollTrigger.create({
-//     trigger: sct,
-//     start: () =>
-//       'top top-=' +
-//       (sct.offsetLeft - window.innerWidth / 2) *
-//         (maxWidth / (maxWidth - window.innerWidth)),
-//     end: () =>
-//       '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
-//     toggleClass: { targets: sct, className: 'active' },
-//   });
-// });
+  tl.to(image, { height: 0 });
+});
 
-smoothScroll('#content');
+gsap.set('.panel-text', { zIndex: (i, target, targets) => targets.length - i });
+
+var texts = gsap.utils.toArray('.panel-text');
+
+texts.forEach((text, i) => {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#slideshow-wrap',
+      scroller: '.scroller',
+      start: () => 'top -' + window.innerHeight * i,
+      end: () => '+=' + window.innerHeight,
+      scrub: true,
+      toggleActions: 'play none reverse none',
+      invalidateOnRefresh: true,
+    },
+  });
+
+  tl.to(text, { duration: 0.33, opacity: 1, y: '50%' }).to(
+    text,
+    { duration: 0.33, opacity: 0, y: '0%' },
+    0.66
+  );
+});
+
+ScrollTrigger.create({
+  trigger: '#slideshow-wrap',
+  scroller: '.scroller',
+  scrub: true,
+  markers: true,
+  pin: true,
+  start: () => 'top top',
+  end: () => '+=' + (images.length + 1) * window.innerHeight,
+  invalidateOnRefresh: true,
+});
 
 // Content Animations
 function animateFrom(elem, direction) {
@@ -82,6 +112,7 @@ uContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'top bottom',
       end: 'bottom center',
       scrub: true,
@@ -104,6 +135,7 @@ lContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'top bottom',
       end: 'center center',
       scrub: true,
@@ -126,6 +158,7 @@ rContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'top bottom',
       end: 'center center',
       scrub: true,
@@ -149,6 +182,7 @@ lOContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'center bottom',
       end: 'bottom top',
       scrub: true,
@@ -172,6 +206,7 @@ rOContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'center bottom',
       end: 'bottom top',
       scrub: true,
@@ -194,6 +229,7 @@ dOContent.forEach((elem, i) => {
   const tl = gsap.timeline({
     scrollTrigger: {
       trigger: container,
+      scroller: '.scroller',
       start: 'center bottom',
       end: 'bottom top',
       scrub: true,
@@ -257,93 +293,3 @@ gsap.utils.toArray('.btn-book').forEach((acrossArrow) => {
   acrossArrow.addEventListener('mouseenter', () => hover.play());
   acrossArrow.addEventListener('mouseleave', () => hover.reverse());
 });
-
-// this is the helper function that sets it all up. Pass in the content <div> and then the wrapping viewport <div> (can be the elements or selector text). It also sets the default "scroller" to the content so you don't have to do that on all your ScrollTriggers.
-function smoothScroll(content, viewport, smoothness) {
-  content = gsap.utils.toArray(content)[0];
-  smoothness = smoothness || 1;
-
-  gsap.set(viewport || content.parentNode, {
-    overflow: 'hidden',
-    position: 'fixed',
-    height: '100%',
-    width: '100%',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-  });
-  gsap.set(content, { overflow: 'visible', width: '100%' });
-
-  let getProp = gsap.getProperty(content),
-    setProp = gsap.quickSetter(content, 'y', 'px'),
-    setScroll = ScrollTrigger.getScrollFunc(window),
-    removeScroll = () => (content.style.overflow = 'visible'),
-    killScrub = (trigger) => {
-      let scrub = trigger.getTween
-        ? trigger.getTween()
-        : gsap.getTweensOf(trigger.animation)[0]; // getTween() was added in 3.6.2
-      scrub && scrub.kill();
-      trigger.animation.progress(trigger.progress);
-    },
-    height,
-    isProxyScrolling;
-
-  function onResize() {
-    height = content.clientHeight;
-    content.style.overflow = 'visible';
-    document.body.style.height = height + 'px';
-  }
-  onResize();
-  ScrollTrigger.addEventListener('refreshInit', onResize);
-  ScrollTrigger.addEventListener('refresh', () => {
-    removeScroll();
-    requestAnimationFrame(removeScroll);
-  });
-  ScrollTrigger.defaults({ scroller: content });
-  ScrollTrigger.prototype.update = (p) => p; // works around an issue in ScrollTrigger 3.6.1 and earlier (fixed in 3.6.2, so this line could be deleted if you're using 3.6.2 or later)
-
-  ScrollTrigger.scrollerProxy(content, {
-    scrollTop(value) {
-      if (arguments.length) {
-        isProxyScrolling = true; // otherwise, if snapping was applied (or anything that attempted to SET the scroll proxy's scroll position), we'd set the scroll here which would then (on the next tick) update the content tween/ScrollTrigger which would try to smoothly animate to that new value, thus the scrub tween would impede the progress. So we use this flag to respond accordingly in the ScrollTrigger's onUpdate and effectively force the scrub to its end immediately.
-        setProp(-value);
-        setScroll(value);
-        return;
-      }
-      return -getProp('y');
-    },
-    getBoundingClientRect() {
-      return {
-        top: 0,
-        left: 0,
-        width: window.innerWidth,
-        height: window.innerHeight,
-      };
-    },
-  });
-
-  return ScrollTrigger.create({
-    animation: gsap.fromTo(
-      content,
-      { y: 0 },
-      {
-        y: () => document.documentElement.clientHeight - height,
-        ease: 'none',
-        onUpdate: ScrollTrigger.update,
-      }
-    ),
-    scroller: window,
-    invalidateOnRefresh: true,
-    start: 0,
-    end: () => height - document.documentElement.clientHeight,
-    scrub: smoothness,
-    onUpdate: (self) => {
-      if (isProxyScrolling) {
-        killScrub(self);
-        isProxyScrolling = false;
-      }
-    },
-    onRefresh: killScrub, // when the screen resizes, we just want the animation to immediately go to the appropriate spot rather than animating there, so basically kill the scrub.
-  });
-}
